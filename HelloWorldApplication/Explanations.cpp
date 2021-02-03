d3630d2bc6006854a4b40006c084179afd535623
DannyVanIets <dannyvanbokhorst@live.nl>
2021-02-03 12:42:59 +0100

    Hello world test application

---
+++ HelloWorldApplication/Explanations.cpp
@@ -0,0 +1,16 @@
+// For compiling
+/*
+ * To compile a file, do "g++ <filename here>", for example g++ spell.cpp.
+ * This will create a file ./a.out.
+ * To run this file, do "./a.out" or "<filename here>" in the console.
+ * To create a specific name for this compiled name, do "g++ <filename here> -o <compile filename here>,
+ * for example "g++ spell.cpp -o spell".
+ * You can then run the file like this: "<compile filename here>", for example: "./spell"
+ */
+// https://www.codecademy.com/courses/learn-c-plus-plus/articles/cpp-compile-execute-locally
+
+/*
+ * For the console, make sure you always put a "\n" after every cout!
+ */
+
+// using namespace std; makes it so that you only need to code "cout << 'hallo!'", instead of "std::cout << 'Hallo!'".
---
+++ HelloWorldApplication/spell.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <iostream>
+
+class spell
+{
+public:
+	void print()
+	{
+		std::cout << "Expecto Patronum\n";
+	}
+};
