3c310aa47f68ea58e42e3a5e98cc75309937df45
DannyVanIets <dannyvanbokhorst@live.nl>
2021-03-30 10:19:41 +0200

    Bit operators een beetje gedaan

--- Voorbeelden/Lesson6Week6/Lesson6Week6.cpp
+++ Voorbeelden/Lesson6Week6/Lesson6Week6.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+const unsigned char road_mask = 8;
+
+bool has_road(unsigned char field)
+{
+	bool has_road = field & road_mask;
+	return has_road;
+}
+
+void make_road(unsigned char &field)
+{
+	// Call by reference?
+	field = field | road_mask;
+}
+
+bool mine_allowed(unsigned char field)
+{
+	return false;
+}
+
+int get_player(unsigned char unit)
+{
+	return 0;
+}
+
 int main()
 {
-    // Use unsigned types. Best use!
+	cout << has_road(4);
+	
 }
